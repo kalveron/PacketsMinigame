1cdbaa938341a585651750d35b92b6dafeb2cd9b
kalveron <andrewmjackman@aol.com>
2022-04-09 14:50:51 -0400

    Create SliderValuePresenter

---
+++ Packets/Assets/Scripts/UI/SliderValuePresenter.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Packets
+{
+    public class SliderValuePresenter : MonoBehaviour
+    {
+        private const string ProgressString = "{0}/100";
+        [SerializeField]
+        private TextMeshProUGUI _text;
+
+        [SerializeField]
+        private Slider _slider;
+
+        private void Start()
+        {
+            UpdateText(_slider.value);
+            _slider.onValueChanged.AddListener(UpdateText);
+        }
+
+       
+        private  void UpdateText(float value)
+        {
+            _text.text = string.Format(ProgressString, value * 10);
+        }
+    }
+}
