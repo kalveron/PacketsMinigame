c69a9402c5479d2ce34deb8fc9e8d69973ab04ce
kalveron <andrewmjackman@aol.com>
2022-04-16 10:31:13 -0400

    Track packet processing

--- Packets/Assets/Scripts/Player/GameManager.cs
+++ Packets/Assets/Scripts/Player/GameManager.cs
@@ -1,29 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Packets
 {
     public class GameManager : MonoBehaviour
     {
-        private const float WaveDelay = 20f;
-
+        private const float ProgressPerPacket = .1f;
 
         [SerializeField]
         private PacketSpawner _spawner;
-        
-        private float _waveCooldown = 0f;
-        
 
-      
+        [SerializeField]
+        private Slider _progressSlider;
+
+        [SerializeField]
+        private Slider _corruptionSlider;
+
+        private float _waveCooldown = 0f;
+        private float _waveDelay;
+
+        private void Start()
+        {
+            Port.PacketProcessed += OnPort_PacketProcessed;
+        }
+
+        private void OnPort_PacketProcessed(PacketProcessedArgs obj)
+        {
+           if(obj.Accepted)
+            {
+                var slider = obj.Packet.Friendly ? _progressSlider : _corruptionSlider;
+
+                slider.value += ProgressPerPacket;
+            }
+        }
+
         void Update()
         {
             _waveCooldown += Time.deltaTime;
-            if(_waveCooldown >= WaveDelay)
+            if(_waveCooldown >= _waveDelay)
             {
-                for(int i = 0; i < _spawner.PacketLimit; i++)
-                {
+               
                     bool friendly = Random.Range(0, 2) == 0;
                     _spawner.SpawnPacket(friendly);
-                }
+                
+                _waveCooldown = 0;
+                _waveDelay = Random.Range(3f, 5f);
             }
         }
     }
